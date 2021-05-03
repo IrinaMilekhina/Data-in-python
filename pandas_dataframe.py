25eee561deb5889f44f2811bab146f10437c9bfe
IrinaMilekhina <i.mezhova@gmail.com>
2021-05-03 23:17:06 +0300

    pandas theory

--- pandas_dataframe.py
+++ pandas_dataframe.py
@@ -20,17 +20,14 @@ Dataframe можно воспринимать как словарь (dict), со
 '''
 
 data = {'color': ['blue', 'green', 'yellow', 'red', 'white'],
-        'object': ['ball', 'pen', 'pencil', 'paper', 'mug'],
+        'objct': ['ball', 'pen', 'pencil', 'paper', 'mug'],
         'price': [12.0, 1.2, 74.9, 0.9, 1.7]}
 frame = pd.DataFrame(data)
-'''
-    color  object  price
-0    blue    ball   12.0
-1   green     pen    1.2
-2  yellow  pencil   74.9
-3     red   paper    0.9
-4   white     mug    1.7
-'''
+print(f'print(frame):\n{frame}\n')
+
+# Для переименования колонки используется метод .rename
+frame.rename(columns={'objct': 'object'}, inplace=True)
+print(f'метод .rename():\n{frame}\n')
 
 '''
 Также для объектов Dataframe если метки явно не заданы в массиве index, pandas автоматически присваивает числовую
@@ -38,89 +35,58 @@ frame = pd.DataFrame(data)
 Вместо использования объекта dict можно определить три аргумента в конструкторе в следующем порядке: 
 матрицу данных, массив значений для параметра index и массив с названиями колонок для параметра columns.
 Колонке с индексами вы также можете дать название следующей командой:
+'''
+frame.columns.name = 'Ind'
+print(f'Index named column:\n{frame}\n')
 
-my_df.columns.name = 'Your name'
+# frame.rename_axis("Ind", axis='columns', inplace=True)
 
+'''
 В большинстве случаев простейший способ создать матрицу значений — использовать np.arrange(16).reshape((4,4)). 
 Это формирует матрицу размером 4х4 из чисел от 0 до 15.
 '''
-
-frame3 = pd.DataFrame(np.arange(16).reshape((4,4)),
+frame2 = pd.DataFrame(np.arange(16).reshape((4,4)),
                       index=['red', 'blue', 'yellow', 'white'],
                       columns=['ball', 'pen', 'pencil', 'paper'])
-'''
-        ball  pen  pencil  paper
-red        0    1       2      3
-blue       4    5       6      7
-yellow     8    9      10     11
-white     12   13      14     15
-'''
+print(f'frame2:\n{frame2}\n')
 
-# print(frame.columns)  --> Index(['color', 'object', 'price'], dtype='object')
-# print(frame.index)  -- RangeIndex(start=0, stop=5, step=1)
-'''
-print(frame.values) -->
+print(f'print(frame.columns):\n{frame.columns}\n')
+print(f'print(frame2.columns):\n{frame2.columns}\n')
+print(f'print(frame.index):\n{frame.index}\n')
+print(f'print(frame2.index):\n{frame2.index}\n')
 
-[['blue' 'ball' 12.0]
- ['green' 'pen' 1.2]
- ['yellow' 'pencil' 74.9]
- ['red' 'paper' 0.9]
- ['white' 'mug' 1.7]]
-'''
+print(f'print(frame.values):\n{frame.values}\n')
+print(f'print(frame2.values):\n{frame2.values}\n')
 
-'''
-Указав в квадратных скобках название колонки, можно получить значений в ней.
-print(frame['price'])  -->
-0    12.0
-1     1.2
-2    74.9
-3     0.9
-4     1.7
-Name: price, dtype: float64
-'''
+# Указав в квадратных скобках название колонки, можно получить значения в ней.
+print(f"print(frame['price']):\n{frame['price']}\n")
 
-'''
-Возвращаемое значение — объект Series. Название колонки можно использовать и в качестве атрибута.
-print(frame.price)  -->
-0    12.0
-1     1.2
-2    74.9
-3     0.9
-4     1.7
-Name: price, dtype: float64
-'''
-'''
-Для строк внутри Dataframe используется атрибут loc со значением индекса нужной строки.
-print(frame.loc[3])
-color       red
-object    paper
-price       0.9
-Name: 3, dtype: object
-'''
+# Возвращаемое значение — объект Series.
+var = frame['price']
+print(f"type(var):\n{type(var)}\n")
+
+# Название колонки можно использовать и в качестве атрибута.
+print(f"print(frame.price):\n{frame.price}\n")
+
+# Для строк внутри Dataframe используется атрибут loc со значением индекса нужной строки.
+print(f"print(frame.loc[3]):\n{frame.loc[3]}\n")
+
 '''
 Возвращаемый объект — это снова Series, где названия колонок — это уже метки массива индексов, 
 а значения — данные Series.
-
 Для выбора нескольких строк можно указать массив с их последовательностью.
-print(frame.loc[[2, 4]])
-    color  object  price
-2  yellow  pencil   74.9
-4   white     mug    1.7
 '''
-'''
-Если необходимо извлечь часть Dataframe с конкретными строками, для этого можно использовать номера индексов. 
-Она выведет данные из соответствующей строки и названия колонок.
-print(frame[0:1])
-  color object  price
-0  blue   ball   12.0
+print(f"print(frame.loc[[2, 4]]):\n{frame.loc[[2, 4]]}\n")
 
-print(frame[1:3])
-    color  object  price
-1   green     pen    1.2
-2  yellow  pencil   74.9
 '''
+Если необходимо извлечь часть Dataframe с конкретными строками, то можно использовать номера индексов. 
+Будут выведены данные из соответствующей строки и названия колонок.
+'''
+print(f"print(frame[0:1]):\n{frame[0:1]}\n")
+print(f"print(frame[1:3]):\n{frame[1:3]}\n")
+
 '''
 Наконец, если необходимо получить одно значение из объекта, сперва нужно указать название колонки, 
 а потом — индекс или метку строки.
-print(frame['object'][3]) --> paper
 '''
+print(f"print(frame['object'][3]):\n{frame['object'][3]}\n")
